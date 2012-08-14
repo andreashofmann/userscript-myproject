299bd0ac30f7757438e2f49a73185e151440d719
Andreas Hofmann <mail@andreashofmann.net>
2012-08-14 08:42:18 +0200

    Added context menu for tasks

--- myproject.js
+++ myproject.js
@@ -331,6 +331,60 @@
                 }
             });
 
+            modules.push({
+                id: 'task-contextmenu',
+                menuItem: 'Context menu for task',
+                onActivate: function () {
+                    jQuery('.task').contextPopup({
+                        items: [{
+                            label: 'Assign to me',
+                            action: function (event) {
+                                var target = $(event.target),
+                                    task;
+
+                                if (!target.hasClass('task')) {
+                                    target = target.parent('.task');
+                                }
+                                task = target.data('this');
+                                task.tempSaveDirectives = task.saveDirectives;
+                                task.saveDirectives = function () {
+                                    var directives = this.tempSaveDirectives();
+                                    directives.data.replace(/&assigned_to_id=[0-9]*/g, '');
+                                    directives.data += '&assigned_to_id=' + MP.helper.getUserId();
+                                    this.$.css('background-color', MP.helper.getUserColor());
+                                    return directives;
+                                };
+                                task.saveEdits();
+                                task.saveDirectives = task.tempSaveDirectives;
+                                delete task.tempSaveDirectives;
+                            }
+                        }, {
+                            label: 'Unassign',
+                            action: function (event) {
+                                var target = $(event.target),
+                                    task;
+
+                                if (!target.hasClass('task')) {
+                                    target = target.parent('.task');
+                                }
+                                task = target.data('this');
+                                task.tempSaveDirectives = task.saveDirectives;
+                                task.saveDirectives = function () {
+                                    var directives = this.tempSaveDirectives();
+                                    directives.data.replace(/&assigned_to_id=[0-9]*/g, '');
+                                    directives.data += '&assigned_to_id=';
+                                    this.$.css('background-color', '');
+                                    return directives;
+                                };
+                                task.saveEdits();
+                                task.saveDirectives = task.tempSaveDirectives;
+                                delete task.tempSaveDirectives;
+                            }
+                        }]
+                    });
+                }
+            });
+
             this.modules = modules;
         },
 
@@ -434,6 +488,117 @@
             }
         }
 
+    };
+
+    /**
+ * jQuery plugin for Pretty looking right click context menu.
+ *
+ * Requires popup.js and popup.css to be included in your page. And jQuery, obviously.
+ *
+ * Usage:
+ *
+ *   $('.something').contextPopup({
+ *     title: 'Some title',
+ *     items: [
+ *       {label:'My Item', icon:'/some/icon1.png', action:function() { alert('hi'); }},
+ *       {label:'Item #2', icon:'/some/icon2.png', action:function() { alert('yo'); }},
+ *       null, // divider
+ *       {label:'Blahhhh', icon:'/some/icon3.png', action:function() { alert('bye'); }},
+ *     ]
+ *   });
+ *
+ * Icon needs to be 16x16. I recommend the Fugue icon set from: http://p.yusukekamiyamane.com/
+ *
+ * - Joe Walnes, 2011 http://joewalnes.com/
+ *   https://github.com/joewalnes/jquery-simple-context-menu
+ *
+ * MIT License: https://github.com/joewalnes/jquery-simple-context-menu/blob/master/LICENSE.txt
+ */
+    jQuery.fn.contextPopup = function(menuData) {
+        // Define default settings
+        var settings = {
+            contextMenuClass: 'contextMenuPlugin',
+            gutterLineClass: 'gutterLine',
+            headerClass: 'header',
+            seperatorClass: 'divider',
+            title: '',
+            items: []
+        };
+
+        // merge them
+        $.extend(settings, menuData);
+
+        // Build popup menu HTML
+        function createMenu(e) {
+            var menu = $('<ul class="' + settings.contextMenuClass + '"><div class="' + settings.gutterLineClass + '"></div></ul>')
+            .appendTo(document.body);
+            if (settings.title) {
+                $('<li class="' + settings.headerClass + '"></li>').text(settings.title).appendTo(menu);
+            }
+            settings.items.forEach(function(item) {
+                if (item) {
+                    var rowCode = '<li><a href="#"><span></span></a></li>';
+                    // if(item.icon)
+                    //   rowCode += '<img>';
+                    // rowCode +=  '<span></span></a></li>';
+                    var row = $(rowCode).appendTo(menu);
+                    if(item.icon){
+                        var icon = $('<img>');
+                        icon.attr('src', item.icon);
+                        icon.insertBefore(row.find('span'));
+                    }
+                    row.find('span').text(item.label);
+                    if (item.action) {
+                        row.find('a').click(function(){ item.action(e); });
+                    }
+                } else {
+                    $('<li class="' + settings.seperatorClass + '"></li>').appendTo(menu);
+                }
+            });
+            menu.find('.' + settings.headerClass ).text(settings.title);
+            return menu;
+        }
+
+        // On contextmenu event (right click)
+        this.bind('contextmenu', function(e) {
+            var menu = createMenu(e)
+            .show();
+
+            var left = e.pageX + 5, /* nudge to the right, so the pointer is covering the title */
+            top = e.pageY;
+            if (top + menu.height() >= $(window).height()) {
+                top -= menu.height();
+            }
+            if (left + menu.width() >= $(window).width()) {
+                left -= menu.width();
+            }
+
+            // Create and show menu
+            menu.css({zIndex:1000001, left:left, top:top})
+            .bind('contextmenu', function() { return false; });
+
+            // Cover rest of page with invisible div that when clicked will cancel the popup.
+            var bg = $('<div></div>')
+            .css({left:0, top:0, width:'100%', height:'100%', position:'absolute', zIndex:1000000})
+            .appendTo(document.body)
+            .bind('contextmenu click', function() {
+                // If click or right click anywhere else on page: remove clean up.
+                bg.remove();
+                menu.remove();
+                return false;
+            });
+
+            // When clicking on a link in menu: clean up (in addition to handlers on link already)
+            menu.find('a').click(function() {
+                bg.remove();
+                menu.remove();
+            });
+
+            // Cancel event, so real browser popup doesn't appear.
+            return false;
+        });
+
+        return this;
     };
 
     MP.run();
